acfb2526f27a0cc5df0cea6100959872401ecc49
Sinewave <sine@sinewave.me>
2020-04-11 10:21:24 -0400

    Added jest-axe

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,20 @@
 import { h } from 'preact';
 import { render } from '@testing-library/preact';
+import { axe, toHaveNoViolations } from 'jest-axe';
 import App from './App';
+
+expect.extend(toHaveNoViolations);
 
 describe('App', () => {
   it('Renders without errors', () => {
     expect(() => {
       render(<App />);
     }).not.toThrowError();
+  });
+
+  it('Has no accessibility errors', async () => {
+    const { container } = render(<App />);
+
+    expect(await axe(container)).toHaveNoViolations();
   });
 });
